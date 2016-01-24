baa46677e2337f7f2642e16004537f2b63e02fc3
MC976 <duythuc28493@gmail.com>
2016-01-25 01:04:09 +0700

    Commit

--- photo-picker-master/PhotoPicker/ViewController.swift
+++ photo-picker-master/PhotoPicker/ViewController.swift
@@ -29,12 +29,23 @@ class ViewController: UIViewController {
         captureSession = AVCaptureSession()
         captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
         
-        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+//        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        let videoDevices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        var captureDevice:AVCaptureDevice! = nil
+        
+        for device in videoDevices{
+            let device = device as! AVCaptureDevice
+            if device.position == AVCaptureDevicePosition.Front {
+                captureDevice = device
+                break
+            }
+        }
         
         var error: NSError?
         var input: AVCaptureDeviceInput!
         do {
-            input = try AVCaptureDeviceInput(device: backCamera)
+            input = try AVCaptureDeviceInput(device: captureDevice)
         } catch let error1 as NSError {
             error = error1
             input = nil
@@ -74,16 +85,91 @@ class ViewController: UIViewController {
                     let dataProvider = CGDataProviderCreateWithCFData(imageData)
                     let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
                     
-                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
-                    self.capturedImage.image = image
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.LeftMirrored)
+//                    self.capturedImage.image = image
+//                    self.capturedImage.image = self.imageEffect(self.capturedImage.image!)
+                    self.capturedImage.image = self.imageEffect(image)
                 }
             })
         }
     }
     
+    func imageEffect(effectImage: UIImage) -> UIImage {
+        var ciImage  = CIImage(CGImage:effectImage.CGImage!)
+        var ciDetector = CIDetector(ofType:CIDetectorTypeFace
+            ,context:nil
+            ,options:[
+                CIDetectorAccuracy:CIDetectorAccuracyHigh,
+                CIDetectorSmile:true
+            ]
+        )
+        var features = ciDetector.featuresInImage(ciImage)
+        
+        UIGraphicsBeginImageContext(effectImage.size)
+        effectImage.drawInRect(CGRectMake(0,0,effectImage.size.width,effectImage.size.height))
+        
+        for feature in features{
+            
+            //context
+            var drawCtxt = UIGraphicsGetCurrentContext()
+            var faceFeature = feature as! CIFaceFeature
+            //face
+            var faceRect = (feature as! CIFaceFeature).bounds
+            faceRect.origin.y = effectImage.size.height - faceRect.origin.y - faceRect.size.height
+            CGContextSetStrokeColorWithColor(drawCtxt, UIColor.redColor().CGColor)
+            CGContextStrokeRect(drawCtxt,faceRect)
+            
+            //mouse
+            if(faceFeature.hasMouthPosition){
+                var mouseRectY = effectImage.size.height - faceFeature.mouthPosition.y
+                var mouseRect  = CGRectMake(faceFeature.mouthPosition.x - 5,mouseRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt,UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,mouseRect)
+            }
+            
+            //hige
+            var higeImg      = UIImage(named:"hige_100.png")
+            var mouseRectY = effectImage.size.height - faceFeature.mouthPosition.y
+            //ヒゲの横幅は顔の4/5程度
+            var higeWidth  = faceRect.size.width * 4/5
+            var higeHeight = higeWidth * 0.3 // 元画像が100:30なのでWidthの30%が縦幅
+            var higeRect  = CGRectMake(faceFeature.mouthPosition.x - higeWidth/2,mouseRectY - higeHeight/2,higeWidth,higeHeight)
+            CGContextDrawImage(drawCtxt,higeRect,higeImg!.CGImage)
+            
+            //leftEye
+            if((faceFeature.hasLeftEyePosition)){
+                var leftEyeRectY = effectImage.size.height - faceFeature.leftEyePosition.y
+                var leftEyeRect  = CGRectMake(faceFeature.leftEyePosition.x - 5,leftEyeRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,leftEyeRect)
+            }
+            
+            //rightEye
+            if((faceFeature.hasRightEyePosition) ){
+                var rightEyeRectY = effectImage.size.height - faceFeature.rightEyePosition.y
+                var rightEyeRect  = CGRectMake(faceFeature.rightEyePosition.x - 5,rightEyeRectY - 5,10,10)
+                CGContextSetStrokeColorWithColor(drawCtxt, UIColor.blueColor().CGColor)
+                CGContextStrokeRect(drawCtxt,rightEyeRect)
+            }
+        }
+        var drawedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return drawedImage
+    }
+    
     @IBAction func didPressTakeAnother(sender: AnyObject) {
         captureSession!.startRunning()
     }
+    
+//    func frontCamera() -> AVCaptureDevice {
+//        let devices = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+//        for device in devices {
+//            let device = device as AVCaptureDevice
+//            if device.position == AVCaptureDevicePosition.Front {
+//                return device
+//            }
+//        }
+//    }
 
 
 }
